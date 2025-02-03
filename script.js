b4a39d99d2dac77958c73fcc4c9814b02517da4b
vanikkanen <vanikkanen@gmail.com>
2025-02-03 18:35:38 +0200

    Add event listener to buttons and remove unneeded game logic

--- script.js
+++ script.js
@@ -11,40 +11,31 @@ function getComputerChoice() {
     }
 }
 
-function getHumanChoice() {
-    let choice = prompt("Please insert your choice from rock, paper and scissors")
-    return choice
-}
+humanScore = 0
+computerScore = 0
 
 
-function playGame() {
-    let humanScore = 0
-    let computerScore = 0
+function playRound(humanChoice, computerChoice) {
 
-    let humanChoice = getHumanChoice()
-    let computerChoice = getComputerChoice()
-    playRound(humanChoice, computerChoice)
-
-
-    function playRound(humanChoice, computerChoice) {
-
-        humanChoice = humanChoice.toLowerCase()
-        // Human wins
-        if (humanChoice === "rock" && computerChoice === "scissors" || humanChoice === "paper" && computerChoice === "rock" || humanChoice === "scissors" && computerChoice === "paper") {
-            humanScore += 1
-            console.log(`You win! ${humanChoice} beats ${computerChoice}`)
-        }
-        else if (computerChoice === "rock" && humanChoice === "scissors" || computerChoice === "paper" && humanChoice === "rock" || computerChoice === "scissors" && humanChoice === "paper") {
-            computerScore += 1
-            console.log(`You lose! ${computerChoice} beats ${humanChoice}`)
-        }
-        else {
-            console.log(`It's a tie! Both chose ${humanChoice}`)
-        }
+    humanChoice = humanChoice.toLowerCase()
+    // Human wins
+    if (humanChoice === "rock" && computerChoice === "scissors" || humanChoice === "paper" && computerChoice === "rock" || humanChoice === "scissors" && computerChoice === "paper") {
+        humanScore += 1
+        console.log(`You win! ${humanChoice} beats ${computerChoice}`)
     }
-
-
-
+    else if (computerChoice === "rock" && humanChoice === "scissors" || computerChoice === "paper" && humanChoice === "rock" || computerChoice === "scissors" && humanChoice === "paper") {
+        computerScore += 1
+        console.log(`You lose! ${computerChoice} beats ${humanChoice}`)
+    }
+    else {
+        console.log(`It's a tie! Both chose ${humanChoice}`)
+    }
 }
 
-playGame()
+const btns = document.querySelectorAll("button")
+
+btns.forEach(btn => {
+    btn.addEventListener("click", () => {
+        playRound(btn.textContent, getComputerChoice())
+    })
+})
